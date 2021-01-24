e3cbd60b8ec8757dfe0492cf9701d93d0712fbde
Russell Gordon <rgordon@lcs.on.ca>
2021-01-24 09:42:40 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Russell Gordon on 2021-01-24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
