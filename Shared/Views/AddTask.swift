a16a0527a45853e3ecc3fa36b41220a1cb57621d
Russell Gordon <rgordon@lcs.on.ca>
2021-01-24 11:36:41 -0500

    Connected ContentView to AddTask view by using a sheet view modifier.

--- Shared/Views/AddTask.swift
+++ Shared/Views/AddTask.swift
@@ -13,6 +13,9 @@ struct AddTask: View {
     @State private var description = ""
     @State private var priority = TaskPriority.low
     
+    // Whether to show this view
+    @Binding var showing: Bool
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -41,13 +44,15 @@ struct AddTask: View {
     
     func saveTask() {
         
-        
+        // Dismiss this view
+        showing = false
+
     }
     
 }
 
 struct AddTask_Previews: PreviewProvider {
     static var previews: some View {
-        AddTask()
+        AddTask(showing: .constant(true))
     }
 }
