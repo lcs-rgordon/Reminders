15c72113e2e6afb8d9817d44b58f25d5bf2c1638
Russell Gordon <rgordon@lcs.on.ca>
2021-02-28 16:59:03 -0500

    List of tasks is now saved to UserDefaults

--- Shared/Model/Task.swift
+++ Shared/Model/Task.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
-class Task: Identifiable, ObservableObject {
+// Identify what properties should be read to and written from JSON
+enum TaskCodingKeys: CodingKey {
+    case description
+    case priority
+    case completed
+}
+
+class Task: Identifiable, ObservableObject, Codable {
     var id = UUID()
     var description: String
     var priority: TaskPriority
@@ -18,6 +25,30 @@ class Task: Identifiable, ObservableObject {
         self.description = description
         self.priority = priority
         self.completed = completed
+    }
+    
+    // Provide details for how to decode from JSON into an instance of this data type
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: TaskCodingKeys.self)
+        
+        // Decode "description" property into an instance of the String type
+        self.description = try container.decode(String.self, forKey: .description)
+        // Decode "priority" property into an instance of the TaskPriority type
+        self.priority = try container.decode(TaskPriority.self, forKey: .priority)
+        // Decode "completed" property into an instance of the Bool type
+        self.completed = try container.decode(Bool.self, forKey: .completed)
+    }
+    
+    // Provide details for how to encode to JSON from an instance of this type
+    func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: TaskCodingKeys.self)
+
+        // Everything is encoded into String types
+        try container.encode(description, forKey: .description)
+        try container.encode(priority.rawValue, forKey: .priority)
+        try container.encode(completed, forKey: .completed)
+        
     }
 }
 
--- Shared/Model/TaskStore.swift
+++ Shared/Model/TaskStore.swift
@@ -9,10 +9,53 @@ import Foundation
 
 class TaskStore: ObservableObject {
 
-    @Published var tasks: [Task]
+    // MARK: Stored properties
+    @Published var tasks: [Task] {
+        
+        // This property observer will fire when a task is added
+        // The existence of this property observer ensures tasks are saved when the app is quit
+        didSet {
+            
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(tasks) {
+                
+                // DEBUG
+                print("Saving tasks list now...")
+                
+                // Actually save the task
+                UserDefaults.standard.setValue(encoded, forKey: "tasks")
+                
+            }
+            
+        }
+        
+    }
     
+    // MARK: Initializer
     init(tasks: [Task] = []) {
-        self.tasks = tasks
+        
+        // Try to read the existing tasks from the app bundle
+        if let readItems = UserDefaults.standard.data(forKey: "tasks") {
+            
+            let decoder = JSONDecoder()
+            
+            // Try to decode the items from JSON
+            // Decodes an instance of the specified type
+            // .self is required to reference the type objecct
+            // So by saying [Task].self we are saying "decode the data from readItems into a structure of type [Task]"
+            if let decoded = try? decoder.decode([Task].self, from: readItems) {
+                self.tasks = decoded
+            } else {
+                self.tasks = []
+            }
+            return
+
+        } else {
+            
+            // If nothing could be loaded from the app bundle, or data could not be decoded, show whatever reminders were passed in to the initializer
+            self.tasks = tasks
+            
+        }
     }
 }
 
