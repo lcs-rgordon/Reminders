758a8c66b4f2c5a18094b0de1e2580e058a88388
Russell Gordon <github@russellgordon.ca>
2022-01-23 16:36:53 -0500

    Make location of Cancel and Save buttons on both platforms consistent.

--- Shared/Views/AddTask.swift
+++ Shared/Views/AddTask.swift
@@ -52,6 +52,18 @@ struct AddTask: View {
         #endif
         .navigationTitle(title)
         .toolbar {
+            
+            #if os(iOS)
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    // Dismiss the sheet by adjusting the "showing"
+                    // property, a derived value, which is bound
+                    // to the "showingAddTask" property from
+                    // ContentView, the source of truth
+                    showing = false
+                }
+            }
+            
             // Using .automatic for placement ensures button appears on macOS
             ToolbarItem(placement: .automatic) {
                 Button("Save") {
@@ -62,8 +74,8 @@ struct AddTask: View {
                 // This prevents the user from saving an empty task.
                 .disabled(description.isEmpty)
             }
-            
-            ToolbarItem(placement: .cancellationAction) {
+            #else
+            ToolbarItem(placement: .automatic) {
                 Button("Cancel") {
                     // Dismiss the sheet by adjusting the "showing"
                     // property, a derived value, which is bound
@@ -72,6 +84,19 @@ struct AddTask: View {
                     showing = false
                 }
             }
+
+            // Using .cancellationAction for placement ensures button appears on macOS
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Save") {
+                    saveTask()
+                }
+                // The button is disabled, and therefore cannot be
+                // pressed, when the description of the task is empty.
+                // This prevents the user from saving an empty task.
+                .disabled(description.isEmpty)
+            }
+
+            #endif
         }
         // Prevents dismissal of the sheet by swiping down
         // If sheet is dismissed this way, data is not saved.
