43be59ebcbdc326c3a37040801e0685b2d5f8e24
Russell Gordon <github@russellgordon.ca>
2022-01-23 15:10:26 -0500

    Added ability to filter based on task priority.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     // Whether to show completed tasks or not
     @State var showingCompletedTasks = true
     
+    // What priority of tasks to show
+    @State private var selectedPriorityForVisibleTasks: VisibleTaskPriority = .all
+    
     // Whether to re-compute the view to show changes the list
     // We never actually show this value, but toggling it
     // from "true" to "false" or vice-versa makes SwiftUI update
@@ -30,23 +33,54 @@ struct ContentView: View {
         // DEBUG: What is the current value of this property?
         let _ = print("listShouldUpdate == \(listShouldUpdate)")
         
-        List {
-            ForEach(store.tasks) { task in
-                if showingCompletedTasks {
-                    // Show all tasks
-                    TaskCell(task: task,
-                             triggerListUpdate: .constant(true))
-                } else {
-                    // Only show incomplete tasks
-                    if task.completed == false {
-                        TaskCell(task: task,
-                                 triggerListUpdate: $listShouldUpdate)
+        VStack {
+            
+            Text("Filter by...")
+                .font(Font.caption.smallCaps())
+                .foregroundColor(.secondary)
+            
+            Picker("Priority", selection: $selectedPriorityForVisibleTasks) {
+                Text(VisibleTaskPriority.all.rawValue).tag(VisibleTaskPriority.all)
+                Text(VisibleTaskPriority.low.rawValue).tag(VisibleTaskPriority.low)
+                Text(VisibleTaskPriority.medium.rawValue).tag(VisibleTaskPriority.medium)
+                Text(VisibleTaskPriority.high.rawValue).tag(VisibleTaskPriority.high)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            List {
+                ForEach(store.tasks) { task in
+                    if showingCompletedTasks {
+                        // Show all tasks
+                        if selectedPriorityForVisibleTasks == .all {
+                            TaskCell(task: task,
+                                     triggerListUpdate: .constant(true))
+                        } else {
+                            if task.priority.rawValue == selectedPriorityForVisibleTasks.rawValue {
+                                TaskCell(task: task,
+                                         triggerListUpdate: .constant(true))
+                            }
+                        }
+                    } else {
+                        // Only show incomplete tasks
+                        if task.completed == false {
+                            
+                            if selectedPriorityForVisibleTasks == .all {
+                                TaskCell(task: task,
+                                         triggerListUpdate: $listShouldUpdate)
+                            } else {
+                                if task.priority.rawValue == selectedPriorityForVisibleTasks.rawValue {
+                                    TaskCell(task: task,
+                                             triggerListUpdate: $listShouldUpdate)
+                                }
+                            }
+                        }
                     }
                 }
+                // View modifier invokes the function
+                .onDelete(perform: store.deleteItems)
+                .onMove(perform: store.moveItems)
             }
-            // View modifier invokes the function
-            .onDelete(perform: store.deleteItems)
-            .onMove(perform: store.moveItems)
         }
         .navigationTitle("Reminders")
         .toolbar {
