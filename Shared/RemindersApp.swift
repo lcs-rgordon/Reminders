d19bca75548001a9e2da72d761b37a692913142c
Russell Gordon <github@russellgordon.ca>
2022-01-24 10:36:18 -0500

    Data now persists (saved to JSON file in Documents directory) when app is backgrounded.

--- Shared/RemindersApp.swift
+++ Shared/RemindersApp.swift
@@ -10,6 +10,10 @@ import SwiftUI
 @main
 struct RemindersApp: App {
     
+    // Detect when app moves between the foreground, background, and inactive states
+    @Environment(\.scenePhase) var scenePhase
+    
+    // Create the source of truth for our list of tasks (create the view model)
     @StateObject private var store = TaskStore(tasks: testData)
     
     var body: some Scene {
@@ -18,5 +22,24 @@ struct RemindersApp: App {
                 ContentView(store: store)
             }
         }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .inactive {
+                
+                print("Inactive")
+                
+            } else if newPhase == .active {
+                
+                print("Active")
+                
+            } else if newPhase == .background {
+                
+                print("Background")
+                
+                // Permanently save the list of tasks
+                store.persistTasks()
+                
+            }
+        }
+        
     }
 }
