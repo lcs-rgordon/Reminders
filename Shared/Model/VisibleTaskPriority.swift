006c80228789ca88b3ab93f57f82856193678877
Russell Gordon <github@russellgordon.ca>
2022-01-24 20:44:44 -0500

    Added a picker to let the user decide what tasks should show based on their priority. TODO: Still need to actually filter the tasks.

---
+++ Shared/Model/VisibleTaskPriority.swift
@@ -0,0 +1,15 @@
+//
+//  VisibleTaskPriority.swift
+//  Reminders (iOS)
+//
+//  Created by Russell Gordon on 2022-01-24.
+//
+
+import Foundation
+
+enum VisibleTaskPriority: String {
+    case high = "High"
+    case medium = "Medium"
+    case low = "Low"
+    case all = "All"
+}
--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     // Whether to show completed tasks or not
     @State var showingCompletedTasks = true
     
+    // What priority of tasks to show
+    @State private var selectedPriorityForVisibleTasks: VisibleTaskPriority = .all
+    
     // Whether to re-compute the view to show changes the list
     // We never actually show this value, but toggling it
     // from "true" to "false" or vice-versa makes SwiftUI update
@@ -30,51 +33,76 @@ struct ContentView: View {
         // Has the list been asked to update?
         let _ = print("listShouldUpdate has been toggled. Current value is: \(listShouldUpdate)")
         
-        List {
-            ForEach(store.tasks) { task in
-                
-                if showingCompletedTasks {
-                    // Show all tasks, completed or incomplete
-                    TaskCell(task: task, triggerListUpdate: .constant(true))
-                } else {
+        // What is the selected priority level for task filtering?
+        let _ = print("Filtering tasks by this priority: \(selectedPriorityForVisibleTasks)")
+        
+        VStack {
+            
+            // Label for picker
+            Text("Filter by...")
+                .font(Font.caption.smallCaps())
+                .foregroundColor(.secondary)
+            
+            // Picker to allow user to select what tasks to show
+            Picker("Priority", selection: $selectedPriorityForVisibleTasks) {
+                Text(VisibleTaskPriority.all.rawValue)
+                    .tag(VisibleTaskPriority.all)
+                Text(VisibleTaskPriority.low.rawValue)
+                    .tag(VisibleTaskPriority.low)
+                Text(VisibleTaskPriority.medium.rawValue)
+                    .tag(VisibleTaskPriority.medium)
+                Text(VisibleTaskPriority.high.rawValue)
+                    .tag(VisibleTaskPriority.high)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            List {
+                ForEach(store.tasks) { task in
                     
-                    // Only show incomplete tasks
-                    if task.completed == false {
-                        TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                    if showingCompletedTasks {
+                        // Show all tasks, completed or incomplete
+                        TaskCell(task: task, triggerListUpdate: .constant(true))
+                    } else {
+                        
+                        // Only show incomplete tasks
+                        if task.completed == false {
+                            TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                        }
+                        
                     }
                     
                 }
+                // View modifier invokes the function on the view model, "store"
+                .onDelete(perform: store.deleteItems)
+                .onMove(perform: store.moveItems)
+            }
+            .navigationTitle("Reminders")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add") {
+                        showingAddTask = true
+                    }
+                }
                 
-            }
-            // View modifier invokes the function on the view model, "store"
-            .onDelete(perform: store.deleteItems)
-            .onMove(perform: store.moveItems)
-        }
-        .navigationTitle("Reminders")
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button("Add") {
-                    showingAddTask = true
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    // Allow user to toggle visibility of tasks based on their completion status
+                    //
+                    //     CONDITION TO EVALUATE       WHEN TRUE               WHEN FALSE
+                    Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
+                        print("Value of showingCompletedTasks was: \(showingCompletedTasks)")
+                        showingCompletedTasks.toggle()
+                        print("Value of showingCompletedTasks is now: \(showingCompletedTasks)")
+                    }
                 }
             }
-            
-            ToolbarItem(placement: .navigationBarLeading) {
-                EditButton()
-            }
-            
-            ToolbarItem(placement: .bottomBar) {
-                // Allow user to toggle visibility of tasks based on their completion status
-                //
-                //     CONDITION TO EVALUATE       WHEN TRUE               WHEN FALSE
-                Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
-                    print("Value of showingCompletedTasks was: \(showingCompletedTasks)")
-                    showingCompletedTasks.toggle()
-                    print("Value of showingCompletedTasks is now: \(showingCompletedTasks)")
-                }
-            }
+            .sheet(isPresented: $showingAddTask) {
+                AddTask(store: store, showing: $showingAddTask)
         }
-        .sheet(isPresented: $showingAddTask) {
-            AddTask(store: store, showing: $showingAddTask)
         }
     }
 }
