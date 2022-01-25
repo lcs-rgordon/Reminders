44c80f9b2818c2b80122796ac227c1ac784ecbf7
Russell Gordon <github@russellgordon.ca>
2022-01-24 20:06:08 -0500

    Added a button to toggle whether completed tasks show up or not. Button text is conditional based on the state of a boolean property.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
     // Controls whether the add task is showing
     @State private var showingAddTask = false
     
+    // Whether to show completed tasks or not
+    @State var showingCompletedTasks = true
+    
     var body: some View {
         List {
             ForEach(store.tasks) { task in
@@ -34,6 +37,17 @@ struct ContentView: View {
             
             ToolbarItem(placement: .navigationBarLeading) {
                 EditButton()
+            }
+            
+            ToolbarItem(placement: .bottomBar) {
+                // Allow user to toggle visibility of tasks based on their completion status
+                //
+                //     CONDITION TO EVALUATE       WHEN TRUE               WHEN FALSE
+                Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
+                    print("Value of showingCompletedTasks was: \(showingCompletedTasks)")
+                    showingCompletedTasks.toggle()
+                    print("Value of showingCompletedTasks is now: \(showingCompletedTasks)")
+                }
             }
         }
         .sheet(isPresented: $showingAddTask) {
