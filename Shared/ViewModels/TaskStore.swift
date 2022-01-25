fa26bbd2151c969bc828f61cb9c2477ff8064c5f
Russell Gordon <github@russellgordon.ca>
2022-01-24 19:56:18 -0500

    Allow items to be moved from one position to another.

--- Shared/ViewModels/TaskStore.swift
+++ Shared/ViewModels/TaskStore.swift
@@ -25,6 +25,16 @@ class TaskStore: ObservableObject {
         tasks.remove(atOffsets: offsets)
     }
     
+    // Invoked to move items around in our list
+    func moveItems(from source: IndexSet, to destination: Int) {
+        // "source" again contains a set of item(s) being moved
+        // "destination" is the location the items are being moved to in the list
+        // These arguments are automatically populated for us by the
+        // .onMove view modifier provided by the SwiftUI framework
+        tasks.move(fromOffsets: source, toOffset: destination)
+    }
+
+    
 }
 
 var testStore = TaskStore(tasks: testData)
