8b5b91d4a89d3e2629e847e67077cfaf0728c5b7
Russell Gordon <github@russellgordon.ca>
2022-01-24 21:31:05 -0500

    Data now persists (saved to JSON file in Documents directory) when app is backgrounded.

--- Shared/ViewModels/TaskStore.swift
+++ Shared/ViewModels/TaskStore.swift
@@ -14,7 +14,34 @@ class TaskStore: ObservableObject {
     
     // MARK: Initializers
     init(tasks: [Task] = []) {
-        self.tasks = tasks
+        
+        // Get a URL that points to the saved JSON data containing our list of tasks
+        let filename = getDocumentsDirectory().appendingPathComponent(savedTasksLabel)
+        print(filename)
+                
+        // Attempt to load from the JSON in the stored / persisted file
+        do {
+            
+            // Load the raw data
+            let data = try Data(contentsOf: filename)
+            
+            // What was loaded from the file?
+            print("Got data from file, contents are:")
+            print(String(data: data, encoding: .utf8)!)
+
+            // Decode the data into Swift native data structures
+            self.tasks = try JSONDecoder().decode([Task].self, from: data)
+            
+        } catch {
+            
+            // What went wrong?
+            print(error.localizedDescription)
+            print("Could not load data from file, initializing with tasks provided to initializer.")
+
+            // Set up list of tasks to whatever was passed to this initializer
+            self.tasks = tasks
+
+        }
     }
     
     // MARK: Functions
@@ -32,6 +59,40 @@ class TaskStore: ObservableObject {
         // These arguments are automatically populated for us by the
         // .onMove view modifier provided by the SwiftUI framework
         tasks.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    // Persist the list of tasks
+    func persistTasks() {
+        
+        // Get a URL that points to the saved JSON data containing our list of tasks
+        let filename = getDocumentsDirectory().appendingPathComponent(savedTasksLabel)
+        
+        // Try to encode the data in our people array to JSON
+        do {
+            // Create an encoder
+            let encoder = JSONEncoder()
+            
+            // Ensure the JSON written to the file is human-readable
+            encoder.outputFormatting = .prettyPrinted
+            
+            // Encode the list of tasks we've collected
+            let data = try encoder.encode(self.tasks)
+            
+            // Actually write the JSON file to the documents directory
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+            
+            // See the data that was written
+            print("Saved data to documents directory successfully.")
+            print("===")
+            print(String(data: data, encoding: .utf8)!)
+            
+        } catch {
+            
+            print(error.localizedDescription)
+            print("Unable to write list of tasks to documents directory in app bundle on device.")
+            
+        }
+        
     }
 
     
